85bee7c65edaa992d85f20d152448e780511eb57
fabriciojallaza <fabriciojallaza@gmail.com>
2022-11-08 17:36:05 -0400

    Project creation

--- restcrud/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestcrudConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restcrud'
--- system_core/models.py
+++ system_core/models.py
@@ -2,18 +2,3 @@ import re
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-
-class User(AbstractBaseUser, PermissionsMixin):
-    """User model"""
-    username = models.CharField(unique=True, max_length=150)
-    email = models.EmailField(max_length=100)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-
-
-class Product(models.Model):
-    """Product Model"""
-    sku = models.CharField(max_length=100, unique=True)
-    name = models.CharField(max_length=200)
-    price = models.FloatField(default=0)
-    brand = models.CharField(max_length=200)
